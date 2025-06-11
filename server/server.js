a310b0ce52f99b71de365785485e216327f6cf69
Sashi Sonkusare <sonkusaresakshibhojraj@gmail.com>
2025-06-12 03:12:55 +0530

    updating access

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "https://cool-station-qa1a.vercel.app", 
-  credentials: true
+  origin: "*", 
 }));
 app.use(express.json());
 
