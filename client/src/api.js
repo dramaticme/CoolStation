5d9544d546baee557a03edd0fcd6ebd9dab4ae61
Sashi Sonkusare <sonkusaresakshibhojraj@gmail.com>
2025-06-11 19:34:27 +0530

    deployement_

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-//const API_BASE_URL = "https://coolstation-backend.onrender.com/api";
+//const API_BASE_URL = "https://coolstation.onrender.com/api";
 const BASE_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
